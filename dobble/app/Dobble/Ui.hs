d9d2e2098848b870a8fa681cb0cdeaf4d9f33058
Sebastian <sebastianpagura@gmail.com>
2023-07-02 18:15:20 -0300

    agregar puntos y otorgar puntos random con enter

--- dobble/app/Dobble/Ui.hs
+++ dobble/app/Dobble/Ui.hs
@@ -7,7 +7,7 @@ import qualified Graphics.Vty as V
 import qualified Graphics.Vty.Attributes as VA
 import Brick (Widget, (<+>), str, withBorderStyle, joinBorders)
 import Brick.Widgets.Center (center)
-import Brick.Widgets.Core (padAll, vBox, hBox, padRight, padLeft, padTopBottom, padLeftRight, Padding(Pad))
+import Brick.Widgets.Core (padAll, vBox, hBox, padRight, padLeft, padTopBottom, padLeftRight, padBottom, Padding(Pad), (<=>))
 import Brick.Widgets.Border (borderWithLabel, vBorder)
 import Brick.Widgets.Border.Style (unicode)
 import System.Random (randomRIO)
@@ -18,7 +18,6 @@ import Data.List ((\\))
 type Symbol = Char
 type Card = [Symbol]
 type Deck = [Card]
-
 
 -- Funcion para hacerle shuffle a una lista
 shuffle :: [a] -> IO [a]
@@ -40,9 +39,6 @@ generateCards n = do
     commonCard <- getNSymbols n (card1 ++ card2) [card1 !! index1, card2 !! index2]
     return (card1, card2, commonCard)
 
-
-
-
 getNSymbols :: Int -> [Symbol] -> [Symbol] -> IO [Symbol]
 getNSymbols n exclusions requiredSymbols = do
   let availableSymbols = emojis \\ exclusions
@@ -52,26 +48,18 @@ getNSymbols n exclusions requiredSymbols = do
   return $ shuffledRequiredSymbols ++ shuffledRemainingSymbols
 
   where
-    --emojis = ['😈', '👀', '🤡', '😍', '🥲', '🤣', '😀', '👾', '🧲', '🎃']
-    --emojis = ['\128512'..'\128591']
-    -- https://unicodemap.org/
-    --emojis = [toEnum 0x2660 .. toEnum 0x26FF]
     emojis = ['A' .. 'Z']
 
 -- Chquea que dos cartas tengan 1 simbolo en comun
 symbolsInCommon :: Card -> Card -> Bool
 symbolsInCommon card1 card2 = length (filter (`elem` card2) card1) == 1
 
--- Función que devuelve True si todas las cartas en el conjunto tienen exactamente una letra en común con todas las demás cartas
-checkDeck :: Deck -> Bool
-checkDeck deck = all (\card -> all (symbolsInCommon card) (filter (/= card) deck)) deck
-
-
 data GameState = GameState {
     cardPlayer1 :: Card,
     cardPlayer2 :: Card,
-    cardCommon :: Card
-    --commonSymbol :: Symbol
+    cardCommon :: Card,
+    player1Points :: Int,
+    player2Points :: Int
 } deriving (Show, Read, Eq, Ord)
 
 initState :: IO GameState
@@ -80,11 +68,17 @@ initState = do
     return $ GameState {
         cardPlayer1 = card1,
         cardPlayer2 = card2,
-        cardCommon = card3
+        cardCommon = card3,
+        player1Points = 0,
+        player2Points = 0
     }
 
-updateState :: GameState -> IO GameState
-updateState s = initState
+updateState :: Int -> GameState -> IO GameState
+updateState winner s = do
+    (card1, card2, card3) <- generateCards 7
+    freshState <- initState
+    let updatedGameState = incrementPlayerPoints winner s
+    return (freshState { cardPlayer1 = card1, cardPlayer2 = card2, cardCommon = card3, player1Points = player1Points updatedGameState, player2Points = player2Points updatedGameState })
 
 dobbleMain :: IO ()
 dobbleMain = do
@@ -107,10 +101,10 @@ handleEvent (T.VtyEvent e) = case e of
     -- Enter para siguiente turno
     V.EvKey V.KEnter      [] -> do
                                     s <- get
-                                    ns <- liftIO $ updateState s
+                                    playerNum <- liftIO $ randomRIO (1 :: Int, 2 :: Int)
+                                    ns <- liftIO $ updateState playerNum s
                                     put ns
     _                        -> BM.continueWithoutRedraw
-
 
 draw :: GameState -> [T.Widget n]
 draw state = return (ui state)
@@ -121,9 +115,9 @@ distFromBorder = 10
 
 ui :: GameState -> T.Widget n
 ui s = 
-    let card1Widget = drawCard (cardPlayer1 s) "Jugador 1"
-        cardCommonWidget = drawCard (cardCommon s) "Carta común"
-        card2Widget = drawCard (cardPlayer2 s) "Jugador 2"
+    let card1Widget = drawCard (cardPlayer1 s) "Jugador 1" (Just (player1Points s))
+        cardCommonWidget = drawCard (cardCommon s) "Carta común" Nothing
+        card2Widget = drawCard (cardPlayer2 s) "Jugador 2" (Just (player2Points s))
         cardsWidget = padRight (Pad distFromBorder) card1Widget  
                       <+> vBorder <+> padLeftRight distFromBorder cardCommonWidget
                       <+> vBorder <+> padLeft (Pad distFromBorder) card2Widget
@@ -133,21 +127,30 @@ ui s =
     borderWithLabel (str "Dobble!") $
     center cardsWidget
 
-drawCard :: Card -> String -> T.Widget n
-drawCard card label=
+drawCard :: Card -> String -> Maybe Int -> T.Widget n
+drawCard card label maybePoints =
     let numSymbols = length card
         numRows = ceiling (sqrt (fromIntegral numSymbols))
         symbolsPerRow = ceiling (fromIntegral numSymbols / fromIntegral numRows)
         symbolWidgets = map drawSymbol card
         rows = splitIntoRows symbolsPerRow symbolWidgets
         paddedSymbolRows = map hBox rows
+        cardWidget = padTopBottom internalPaddingY $ padLeftRight internalPaddingX $ vBox paddedSymbolRows
+        cardWithLabel = borderWithLabel (str label) cardWidget
+        cardWithPoints = maybe cardWithLabel (\points -> cardWithLabel <=> str (" (" ++ show points ++ " points)")) maybePoints
+        cardWithTopLabel = padBottom (Pad internalPaddingY) cardWithPoints
     in
-    borderWithLabel (str label) $ padTopBottom internalPaddingY $ padLeftRight internalPaddingX $ vBox paddedSymbolRows
+    padLeftRight internalPaddingX cardWithTopLabel
 
 splitIntoRows :: Int -> [T.Widget n] -> [[T.Widget n]]
 splitIntoRows _ [] = []
 splitIntoRows n xs = take n xs : splitIntoRows n (drop n xs)
 
 drawSymbol :: Symbol -> T.Widget n
-drawSymbol symbol = str([symbol]) <+> str " "
+drawSymbol symbol = str [symbol] <+> str " "
 
+incrementPlayerPoints :: Int -> GameState -> GameState
+incrementPlayerPoints playerNum gameState
+    | playerNum == 1 = gameState { player1Points = player1Points gameState + 1 }
+    | playerNum == 2 = gameState { player2Points = player2Points gameState + 1 }
+    | otherwise = gameState
