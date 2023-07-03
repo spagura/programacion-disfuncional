1fe6043c19c6d8bdcf9f5b5d794ce879b48e4257
Francisco Sicardi <francisco.sicardi@mercadolibre.com>
2023-07-03 15:00:48 -0300

    wire game logic to interface

--- dobble/app/Dobble/Ui.hs
+++ dobble/app/Dobble/Ui.hs
@@ -7,9 +7,9 @@ import qualified Brick.AttrMap as A
 import qualified Graphics.Vty as V
 import qualified Graphics.Vty.Attributes as VA
 import Brick (Widget, (<+>), str, withBorderStyle, joinBorders)
-import Brick.Widgets.Center (center)
+import Brick.Widgets.Center (center, hCenter)
 import Brick.Widgets.Core (padAll, vBox, hBox, padRight, padLeft, padTopBottom, padLeftRight, padBottom, Padding(Pad), (<=>))
-import Brick.Widgets.Border (borderWithLabel, vBorder)
+import Brick.Widgets.Border (borderWithLabel, vBorder, hBorder)
 import Brick.Widgets.Border.Style (unicode)
 import System.Random (randomRIO)
 import Control.Monad.State
@@ -57,7 +57,7 @@ getNSymbols n exclusions requiredSymbols = do
 symbolsInCommon :: Card -> Card -> Bool
 symbolsInCommon card1 card2 = length (filter (`elem` card2) card1) == 1
 
-data MatchResult = Match | NoMatch
+data MatchResult = Match | NoMatch deriving Show
 
 instance Semigroup MatchResult where
     (<>) :: MatchResult -> MatchResult -> MatchResult
@@ -73,23 +73,36 @@ computeMatchResult s1 s2
     | s1 == s2 = Match
     | otherwise = NoMatch
 
+matchSymbol :: Symbol -> Card -> MatchResult
+matchSymbol symbol card = foldMap (\ a -> computeMatchResult a symbol) card
 
-matchSymbol :: (Foldable t) => t Symbol -> Symbol -> Writer [Symbol] MatchResult
-matchSymbol symbols common = foldl (\acc x -> do
+matchCard :: (Foldable t) => t Symbol -> Card -> Writer [Symbol] MatchResult
+matchCard playerCard commonCard = foldl (\acc x -> do
     current <- acc
-    return $ let
-        matchResult = computeMatchResult x common
-        in current <> matchResult) (return NoMatch) symbols
+    case (current) of
+        (Match) -> return Match
+        (NoMatch) -> do
+            tell [x]
+            return $ let matchResult = matchSymbol x commonCard
+                in current <> matchResult) (return NoMatch) playerCard
 
-matchCard :: (Foldable t) => t Symbol -> [Symbol] -> [Writer [Symbol] MatchResult]
-matchCard card commonCard = map (\a -> matchSymbol card a) commonCard
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
+mapTuple f (a1, a2) = (f a1, f a2)
+
+play :: Card -> Card -> Card -> (Card, Card)
+play c1 c2 cCommon = let
+    c1' = c1 -- or other foldable implementation
+    c2' = c2 -- or other foldable implementation
+    in  mapTuple (snd . runWriter)(matchCard c1' cCommon, matchCard c2' cCommon)
 
 data GameState = GameState {
     cardPlayer1 :: Card,
     cardPlayer2 :: Card,
     cardCommon :: Card,
     player1Points :: Int,
-    player2Points :: Int
+    player2Points :: Int,
+    usedPlayer1 :: Card,
+    usedPlayer2 :: Card
 } deriving (Show, Read, Eq, Ord)
 
 initState :: IO GameState
@@ -100,17 +113,26 @@ initState = do
         cardPlayer2 = card2,
         cardCommon = card3,
         player1Points = 0,
-        player2Points = 0
+        player2Points = 0,
+        usedPlayer1 = [],
+        usedPlayer2 = []
     }
-    
 
-updateState :: Int -> GameState -> IO GameState
-updateState winner s = do
-    -- let (viewed1, viewed2) = play (cardPlayer1 s) (cardPlayer2 s) (cardCommon s)
+findWinner :: Card -> Card -> Int
+findWinner c1 c2
+    |  l1 == l2 = 0 -- tied, no winner
+    |  l1 < l2 = 1
+    |  l1 > l2 = 2
+    where l1 = length c1
+          l2 = length c2
+
+updateState :: GameState -> IO GameState
+updateState s = do
+    let (used1, used2) = play (cardPlayer1 s) (cardPlayer2 s) (cardCommon s)
     (card1, card2, card3) <- generateCards 7
     freshState <- initState
-    let updatedGameState = incrementPlayerPoints winner s
-    return (freshState { cardPlayer1 = card1, cardPlayer2 = card2, cardCommon = card3, player1Points = player1Points updatedGameState, player2Points = player2Points updatedGameState })
+    let updatedGameState = incrementPlayerPoints (findWinner used1 used2) s
+    return (freshState { usedPlayer1 = used1, usedPlayer2 = used2, cardPlayer1 = card1, cardPlayer2 = card2, cardCommon = card3, player1Points = player1Points updatedGameState, player2Points = player2Points updatedGameState })
 
 dobbleMain :: IO ()
 dobbleMain = do
@@ -133,8 +155,7 @@ handleEvent (T.VtyEvent e) = case e of
     -- Enter para siguiente turno
     V.EvKey V.KEnter      [] -> do
                                     s <- get
-                                    playerNum <- liftIO $ randomRIO (1 :: Int, 2 :: Int)
-                                    ns <- liftIO $ updateState playerNum s
+                                    ns <- liftIO $ updateState s
                                     put ns
     _                        -> BM.continueWithoutRedraw
 
@@ -147,9 +168,9 @@ distFromBorder = 10
 
 ui :: GameState -> T.Widget n
 ui s = 
-    let card1Widget = drawCard (cardPlayer1 s) "Jugador 1" (Just (player1Points s))
+    let card1Widget = drawCard (cardPlayer1 s) "Jugador 1" (Just ((player1Points s, usedPlayer1 s)))
         cardCommonWidget = drawCard (cardCommon s) "Carta común" Nothing
-        card2Widget = drawCard (cardPlayer2 s) "Jugador 2" (Just (player2Points s))
+        card2Widget = drawCard (cardPlayer2 s) "Jugador 2" (Just ((player2Points s, usedPlayer2 s)))
         cardsWidget = padRight (Pad distFromBorder) card1Widget  
                       <+> vBorder <+> padLeftRight distFromBorder cardCommonWidget
                       <+> vBorder <+> padLeft (Pad distFromBorder) card2Widget
@@ -159,20 +180,24 @@ ui s =
     borderWithLabel (str "Dobble!") $
     center cardsWidget
 
-drawCard :: Card -> String -> Maybe Int -> T.Widget n
-drawCard card label maybePoints =
+drawCard :: Card -> String -> Maybe (Int, Card) -> T.Widget n
+drawCard card label maybePlayed =
     let numSymbols = length card
         numRows = ceiling (sqrt (fromIntegral numSymbols))
         symbolsPerRow = ceiling (fromIntegral numSymbols / fromIntegral numRows)
         symbolWidgets = map drawSymbol card
         rows = splitIntoRows symbolsPerRow symbolWidgets
         paddedSymbolRows = map hBox rows
-        cardWidget = padTopBottom internalPaddingY $ padLeftRight internalPaddingX $ vBox paddedSymbolRows
+        cardWidget = hCenter $ vBox paddedSymbolRows
         cardWithLabel = borderWithLabel (str label) cardWidget
-        cardWithPoints = maybe cardWithLabel (\points -> cardWithLabel <=> str (" (" ++ show points ++ " points)")) maybePoints
+        -- cardWithPoints = maybe cardWithLabel (\points -> cardWithLabel <=> str (" (" ++ show points ++ " points)")) maybePoints
+        cardWithPoints = maybe cardWithLabel (\played -> cardWithLabel <=> displayPlayed played) maybePlayed
         cardWithTopLabel = padBottom (Pad internalPaddingY) cardWithPoints
     in
     padLeftRight internalPaddingX cardWithTopLabel
+
+displayPlayed :: (Int, Card) -> T.Widget n
+displayPlayed (points, used) = vBox $ map hCenter [hBorder, str (" (" ++ show points ++ " points)"), hBorder, str used]
 
 splitIntoRows :: Int -> [T.Widget n] -> [[T.Widget n]]
 splitIntoRows _ [] = []
