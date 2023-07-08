373334aa14da17840f98e40ff281e4e3a785c300
Francisco Sicardi <francisco.sicardi@mercadolibre.com>
2023-07-08 12:27:53 -0300

    add reverse

--- dobble/app/Dobble/Ui.hs
+++ dobble/app/Dobble/Ui.hs
@@ -92,8 +92,8 @@ mapTuple f (a1, a2) = (f a1, f a2)
 
 play :: Card -> Card -> Card -> (Card, Card)
 play c1 c2 cCommon = let
-    c1' = c1 
-    c2' = c2 
+    c1' = c1 -- or other foldable implementation
+    c2' = reverse c2 -- or other foldable implementation
     in  mapTuple (snd . runWriter)(matchCard c1' cCommon, matchCard c2' cCommon)
 
 data GameState = GameState {
